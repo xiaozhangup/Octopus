03e5093e95cce25f390cd6c4c098f75096e3e29b
xiaozhangup <xiaozhangshiw@163.com>
2026-05-11 02:21:39 +0800

    提供更好的世界卡顿报告

--- octopus-server/src/main/java/me/xiaozhangup/octopus/WorldTickFutureTask.java
+++ octopus-server/src/main/java/me/xiaozhangup/octopus/WorldTickFutureTask.java
@@ -8,6 +8,7 @@ import java.util.concurrent.FutureTask;
 
 public class WorldTickFutureTask extends FutureTask<Pair<ServerLevel, Throwable>> {
     private final ServerLevel level;
+    private volatile Thread runningThread;
 
     public WorldTickFutureTask(ServerLevel level, Callable<Pair<ServerLevel, Throwable>> callable) {
         super(callable);
@@ -16,6 +17,36 @@ public class WorldTickFutureTask extends FutureTask<Pair<ServerLevel, Throwable>
 
     public String getLevelName() {
         return level.dimension().identifier().toDebugFileName();
+    }
+
+    public String createTimeoutReport() {
+        final Thread runningThread = this.runningThread;
+        if (runningThread == null) {
+            return "World tick task has not completed after 2 seconds: " + this.getLevelName();
+        }
+
+        final StringBuilder stack = new StringBuilder();
+        stack.append("World tick task has not completed after 2 seconds: ")
+            .append(this.getLevelName())
+            .append(System.lineSeparator())
+            .append("Thread: ")
+            .append(runningThread.getName())
+            .append(" | State: ")
+            .append(runningThread.getState());
+        for (final StackTraceElement element : runningThread.getStackTrace()) {
+            stack.append(System.lineSeparator()).append("\tat ").append(element);
+        }
+        return stack.toString();
+    }
+
+    @Override
+    public void run() {
+        this.runningThread = Thread.currentThread();
+        try {
+            super.run();
+        } finally {
+            this.runningThread = null;
+        }
     }
 
     @Override
